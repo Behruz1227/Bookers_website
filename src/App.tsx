1d10712d9074ae3362ed9dc2c8ed53e3f16ebe37
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-12-21 17:24:22 +0500

    Project start

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold underline text-red-500">
+        Tailwind CSS ishlayapti! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Barcha komponentlar uchun Tailwindni faollashtiring
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
