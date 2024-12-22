f1a9361e7cda9992c327c4f58e47f363909f4ad6
Sardor <sardorbekeshmatov79@gmail.com>
2024-12-22 16:13:30 +0500

    Route olib tashlandi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import PhoneNumber from "./components/Modals/phoneModal";
-
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-        </Routes>
-      </BrowserRouter>
     </>
   );
 }
