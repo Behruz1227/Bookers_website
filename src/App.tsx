dcc20d137706751f6e4da69b79067ff123507bca
dilbarqoldosheva <bintushokir07@gmail.com>
2024-12-23 16:51:21 +0500

    footer links

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import TermsOfUse from "./components/MenuBar";
-// import StandartPlan from "./components/Accardion";
-// import CalendarMultiSelect from "./components/Calendar";
-// import TimePicker from "./components/Time";
+
+
 import Home from "./page/home";
 import Footer from "./components/footer/Footer";
 
+//foooter links
+import Условия from "./page/Условия использования"
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          {/* <Route path="/" element={<TermsOfUse />} />
-          <Route path="/accardion" element={<StandartPlan />} />
-          <Route path="/calendar" element={<CalendarMultiSelect />} />
-          <Route path="/time" element={<TimePicker />} /> */}
-         <Footer/>
+          <Route path="/" element={<Home />} />
+          <Route path="/Условия использования" element={<Условия/>}/>
+        
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
