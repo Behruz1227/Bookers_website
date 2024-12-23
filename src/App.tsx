e0018c2c118c3ba94b39e4bd188f00714129584e
azizbekanvarov <azizbekanvarov321@gmail.com>
2024-12-23 15:18:33 +0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import TermsOfUse from "./components/MenuBar";
-import StandartPlan from "./components/Accardion";
-import CalendarMultiSelect from "./components/Calendar";
-import TimePicker from "./components/Time";
+// import TermsOfUse from "./components/MenuBar";
+// import StandartPlan from "./components/Accardion";
+// import CalendarMultiSelect from "./components/Calendar";
+// import TimePicker from "./components/Time";
+import Home from "./page/home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<TermsOfUse />} />
-          <Route path="/accardion" element={<StandartPlan />} />
+          <Route path="/" element={<Home/>}/>
+          {/* <Route path="/" element={<TermsOfUse />} /> */}
+          {/* <Route path="/accardion" element={<StandartPlan />} />
           <Route path="/calendar" element={<CalendarMultiSelect />} />
-          <Route path="/time" element={<TimePicker />} />
+          <Route path="/time" element={<TimePicker />} /> */}
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/page/home.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/footer/Footer"
+
+
+function Home() {
+  return (
+    <div>
+
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
