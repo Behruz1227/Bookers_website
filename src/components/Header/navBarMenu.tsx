ca49f6200efbd43aedf719f8804d70e0703f1a14
Eshmurodov-D <eshmurodovdilshod017@gmail.com>
2024-12-24 17:39:46 +0500

    kichkina o'zgarishcha

--- src/components/Header/navBarMenu.tsx
+++ src/components/Header/navBarMenu.tsx
@@ -14,7 +14,7 @@ export const Bookers: MenuProps['items'] = [
         key: '2',
         label: (
             <div>
-                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">О компании</Link>
+                <Link to="/AboutCompany" className="hover:text-[#9C0B35] text-[#21212E]">О компании</Link>
             </div>
         ),
         children: [
