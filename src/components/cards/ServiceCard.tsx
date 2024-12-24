13a48fa90943117ffb15ec0cc1cfccae70021e8e
dilbarqoldosheva <bintushokir07@gmail.com>
2024-12-24 16:41:34 +0500

    add

--- src/components/cards/ServiceCard.tsx
+++ src/components/cards/ServiceCard.tsx
@@ -1,17 +1,21 @@
 interface ServiceCardProps {
-    icon: React.ReactNode;
+   image: string;
     title: string;
     description: string;
     className?: string;
   }
   
-  export function ServiceCard({ icon, title, description, className = '' }: ServiceCardProps) {
+  export function ServiceCard({ image, title, description, className = '' }: ServiceCardProps) {
     return (
       <div className={`rounded-lg ${className}`}>
         <div className="bg-gradient-to-b bg-inherit text-white p-6 rounded-lg transition-transform ">
           <div className="flex flex-col items-center text-center space-y-4">
             <div className="w-16 h-16 rounded-full bg-[#9B1B47] flex items-center justify-center">
-              {icon}
+            <img
+              src={image}
+              alt={title}
+
+            />
             </div>
             <h3 className="text-[24px] font-bold font-manrope">{title}</h3>
             <p className="text-[#B9B9C9] text-[18px] font-manrope leading-[27px]">{description}</p>
