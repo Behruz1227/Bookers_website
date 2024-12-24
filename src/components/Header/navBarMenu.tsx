d4ab664a3a47ecee2b59f84117b132defb994f78
Eshmurodov-D <eshmurodovdilshod017@gmail.com>
2024-12-24 14:56:50 +0500

    header yuklandi...

---
+++ src/components/Header/navBarMenu.tsx
@@ -0,0 +1,125 @@
+import { MenuProps } from "antd";
+import { Link } from "react-router-dom";
+
+export const Bookers: MenuProps['items'] = [
+    {
+        key: '1',
+        label: (
+            <div>
+                <Link to="/salom" className="hover:text-[#9C0B35] text-[#21212E]">О продукте</Link>
+            </div>
+        ),
+    },
+    {
+        key: '2',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">О компании</Link>
+            </div>
+        ),
+        children: [
+            {
+                key: '2-1',
+                label: (
+                    <div>
+                        <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Нормативные права</Link>
+                    </div>
+                ),
+            },
+            {
+                key: '2-2',
+                label: (
+                    <div>
+                        <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Наша миссия</Link>
+                    </div>
+                ),
+            },
+            {
+                key: '2-3',
+                label: (
+                    <div>
+                        <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Команда</Link>
+                    </div>
+                ),
+            },
+        ],
+    },
+    {
+        key: '3',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Стандартизация / Безопасность</Link>
+            </div>
+        ),
+
+    },
+    {
+        key: '4',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Вакансии</Link>
+            </div>
+        ),
+
+    }
+];
+
+export const Бронирование: MenuProps['items'] = [
+    {
+        key: '1',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Парикмахерские услуги</Link>
+            </div>
+        ),
+    },
+    {
+        key: '2',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Ногтевой сервис</Link>
+            </div>
+        ),
+    },
+    {
+        key: '3',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Ресницы и брови</Link>
+            </div>
+        ),
+
+    },
+    {
+        key: '4',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Макияж</Link>
+            </div>
+        ),
+    },
+    {
+        key: '5',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Эпиляция</Link>
+            </div>
+        ),
+    },
+    {
+        key: '6',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Косметологические услуги</Link>
+            </div>
+        ),
+    },
+    {
+        key: '7',
+        label: (
+            <div>
+                <Link to="/" className="hover:text-[#9C0B35] text-[#21212E]">Боди-арт</Link>
+            </div>
+        ),
+    }
+];
